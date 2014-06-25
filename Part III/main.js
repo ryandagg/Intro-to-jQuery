41e52e5789f8f09fce3ef3185857059eab3ca336
ryandagg <ryan.dagg@gmail.com>
2014-06-25 15:46:51 -0600

    FINAL

--- Part III/main.js
+++ Part III/main.js
@@ -3,8 +3,12 @@ $(document).on('ready', function() {
 		// how to do this?
 		// create an element and specifiy stuff, including css
 		// first create the div, then worry about position: fixed, then close button
-		$("body").append("<div class ='popup'>stuff</div>")
+		$("body").append("<div class ='popup'>stuff<button class ='closeButton'>Close</button></div>")
 		$(".popup").css({'background-color': 'grey', 'position': 'fixed', "width": "90%", 
 			"height": "90%", "top": "30px", "left": "30px"})
+		$(".closeButton").click(function(){
+			$(".popup").remove();
+		})
 	});
+
 })	
